0fad6073494dedef2e2ad304351ae17251aaaea0
SoraHirasawa <89620534+SoraHirasawa@users.noreply.github.com>
2023-11-03 19:03:20 -0400

    Website Uploaded!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,231 @@
+'use client';
+
 import Image from 'next/image'
+import { Button, Card, List, ListItem, ListItemText, Paper } from '@mui/material';
+import { red } from '@mui/material/colors';
+import { useState } from 'react';
+
+const ingredients = ["Bee", "Bear", "Chicks", "Cloud", "Dino", "Flower", "Frog", "Heart", "Milk", "Milk (Strawberry)", "Mushroom", "Pig",
+                   "Pineapples", "Planet", "Soda", "Star", "Sugar", "Tapioca"];
+
+const recipes = new Map();
+
+recipes.set("Bear Boba", ["Bear", "Bear", "Tapioca"]);
+recipes.set("Bee Boba", ["Bee", "Bee", "Tapioca"]);
+recipes.set("Black and White Boba", ["Cloud", "Tapioca", "Milk"]);
+recipes.set("Boba Cheese Foam", ["Sugar", "Milk (Strawberry)", "Tapioca"]);
+recipes.set("Brown Cat Boba", ["Soda", "Milk", "Tapioca"]);
+recipes.set("Chick Boba", ["Chick", "Chick", "Tapioca"]);
+recipes.set("Cloud Boba", ["Cloud", "Cloud", "Tapioca"]);
+recipes.set("Deer Boba", ["Mushroom", "Flower", "Tapioca"]);
+recipes.set("Diamond Boba", ["Planot", "Dino", "Tapioca"]);
+recipes.set("Dinosaur Boba", ["Dino", "Dino", "Tapioca"]);
+recipes.set("Dragon Boba", ["Cloud", "Dino", "Tapioca"]);
+recipes.set("Dragon Cream", ["Dino", "Cloud", "Milk"]);
+recipes.set("Dragon Fruit Boba", ["Dino", "Mushroom", "Tapioca"]);
+recipes.set("Flower Boba", ["Flower", "Flower", "Tapioca"]);
+recipes.set("Flower Syrup", ["Sugar", "Sugar", "Flower"]);
+recipes.set("Frog Boba", ["Frog", "Frog", "Tapioca"]);
+recipes.set("Galaxy Boba", ["Star", "Planet", "Tapioca"]);
+recipes.set("Glitter Syrup", ["Star", "Star", "Cloud"]);
+recipes.set("Gold Flakes & Whipped Cream", ["Milk (Strawberry)", "Star", "Bee"]);
+recipes.set("Gummy Bears", ["Bear", "Bear", "Bear"]);
+recipes.set("Heart Boba", ["Heart", "Heart", "Tapioca"]);
+recipes.set("Honey", ["Bee", "Bee", "Bee"]);
+recipes.set("Ice Cream Boba", ["Sugar", "Milk", "Tapioca"]);
+recipes.set("Lychee Soda", ["Soda", "Soda", "Soda"]);
+recipes.set("Marshmallows", ["Chick", "Chick", "Sugar"]);
+recipes.set("Moon Boba", ["Heart", "Planet", "Tapioca"]);
+recipes.set("Mushroom Boba", ["Heart", "Mushroom", "Tapioca"]);
+recipes.set("Mushroom Jelly", ["Mushroom", "Mushroom", "Sugar"]);
+recipes.set("Oreo Creme Brulee", ["Sugar", "Milk", "Milk"]);
+recipes.set("Panda Bear Boba", ["Bear", "Bee", "Tapioca"]);
+recipes.set("Pig Boba", ["Pig", "Pig", "Tapioca"]);
+recipes.set("Pineapple", ["Pineapple", "Pineapple", "Pineapple"]);
+recipes.set("Pineapple Jelly", ["Pineapple", "Pineapple", "Sugar"]);
+recipes.set("Pink Dinosaur Boba", ["Dino", "Heart", "Tapioca"]);
+recipes.set("Polar Bear Boba", ["Bear", "Soda", "Tapioca"]);
+recipes.set("Polar Bear Syrup", ["Bear", "Soda", "Sugar"]);
+recipes.set("Planet Boba", ["Planet", "Planet", "Tapioca"]);
+recipes.set("Puppy Boba", ["Bear", "Chick", "Tapioca"]);
+recipes.set("Rainbow Heart Boba", ["Heart", "Star", "Tapioca"]);
+recipes.set("Rainbow Syrup", ["Heart", "Star", "Sugar"]);
+recipes.set("Rose Petals", ["Flower", "Flower", "Flower"]);
+recipes.set("Rose Petals Syrup", ["Flower", "Sugar", "Bee"]);
+recipes.set("Sakura Petals", ["Flower", "Flower", "Heart"]);
+recipes.set("Sakura Petals Soda", ["Flower", "Flower", "Bee"]);
+recipes.set("Sprinkles", ["Bee", "Heart", "Pineapple"]);
+recipes.set("Sprinkles & Whipped Cream", ["Milk (Strawberry)", "Sugar", "Sugar"]);
+recipes.set("Star Boba", ["Star", "Star", "Tapioca"]);
+recipes.set("Strawberry Cow Boba", ["Soda", "Bee", "Tapioca"]);
+recipes.set("Strawberry Foam", ["Milk (Strawberry)", "Sugar", "Flower"]);
+recipes.set("Strawberry Milk", ["Milk (Strawberry)", "Milk (Strawberry)", "Milk (Strawberry)"]);
+recipes.set("Strawberries & Whipped Cream", ["Milk (Strawberry)", "Flower", "Sugar"]);
+recipes.set("Sunflower Boba", ["Star", "Flower", "Tapioca"]);
+recipes.set("Unicorn Boba", ["Soda", "Tapioca", "Soda"]);
+recipes.set("White Cat Boba", ["Soda", "Tapioca", "Heart"]);
+recipes.set("Whipped Cream", ["Milk", "Milk", "Milk"]);
+
+
+
 
 export default function Home() {
+  const [availableIngredients, setIngredients] = useState<Map<string, number>>(new Map());
+
+  // Function to add a new key-value pair
+  const addPair = (key: string, value: number) => {
+    const newPairs = new Map(availableIngredients);
+    newPairs.set(key, value);
+    setIngredients(newPairs);
+  };
+
+  const updatePair = (key: string) => {
+    setIngredients((prevIngredients) => {
+      const newIngredients = new Map(prevIngredients);
+      newIngredients.set(key, (newIngredients.get(key) || 0) + 1);
+      return newIngredients;
+    });
+  }
+
+  const decreasePair = (key: string) => {
+    setIngredients((prevIngredients) => {
+      const newIngredients = new Map(prevIngredients);
+      newIngredients.set(key, (newIngredients.get(key) || 0) - 1);
+      if ((newIngredients.get(key) || 1) < 1) {
+        deletePair(key);
+      }
+      return newIngredients;
+    });
+    
+  }
+
+  const deletePair = (key: string) => {
+    setIngredients((prevIngredients) => {
+      const newIngredients = new Map(prevIngredients);
+      newIngredients.delete(key);
+      return newIngredients;
+    });
+  }
+
+  const allAvailable = (ingredient1: string, ingredient2: string, ingredient3: string) => {
+    return availableIngredients.has(ingredient1) && availableIngredients.has(ingredient2) && availableIngredients.has(ingredient3);
+  }
+
+  const hasEnoughIngredients = (ingredient1: string, ingredient2: string, ingredient3: string) => {
+    const ingredientsInRecipe = new Map();
+    ingredientsInRecipe.set(ingredient1, 1);
+    if (ingredientsInRecipe.has(ingredient2)) {
+      ingredientsInRecipe.set(ingredient2, ingredientsInRecipe.get(ingredient2) + 1);
+    }
+    else {
+      ingredientsInRecipe.set(ingredient2, 1);
+    }
+    if (ingredientsInRecipe.has(ingredient3)) {
+      ingredientsInRecipe.set(ingredient3, ingredientsInRecipe.get(ingredient3) + 1);
+    }
+    else {
+      ingredientsInRecipe.set(ingredient3, 1);
+    }
+    return (ingredientsInRecipe.get(ingredient1) <= (availableIngredients.get(ingredient1) || 0) && 
+    ingredientsInRecipe.get(ingredient2) <= (availableIngredients.get(ingredient2) || 0) &&
+    ingredientsInRecipe.get(ingredient3) <= (availableIngredients.get(ingredient3) || 0))
+  }
+
+  const recipeIsRelevant = (ingredient1: string, ingredient2: string, ingredient3: string) => {
+    if (allAvailable(ingredient1, ingredient2, ingredient3)) {
+      return (hasEnoughIngredients(ingredient1, ingredient2, ingredient3))
+    }
+    return false;
+  }
+
+  const displayRecipes = () => {
+    const availableRecipes = Array.from(recipes).map(([recipeName, ingredients]) => {
+      if (recipeIsRelevant(ingredients[0], ingredients[1], ingredients[2])) {
+        return recipeName;
+      }
+      return null;
+    });
+  
+    return availableRecipes.filter((recipe) => recipe !== null);
+  }
+  
+  
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <title>Magic Den Recipe Finder</title>
+      <div className='fixed top-0 left-0 p-10 text-4xl font-sans'>
+        <h1> Magic Den Recipe Finder</h1>
+      </div>
+      <div className='absolute overflow-y-scroll fixed left-0 p-10 flex flex-row'>
+        <Card className='p-1'>
+          <List>
+            {ingredients.map((ingredient) => (
+            <ListItem className="p-5" key={ingredient}>
+              {ingredient}
+              <Button
+                onClick={() => {
+                  if (availableIngredients.has(ingredient)) {
+                    updatePair(ingredient);
+                  }
+                  else {
+                    addPair(ingredient, 1);
+                  }
+                }} 
+                variant="outlined"
+                sx={ {borderRadius: 10}}>
+                +
+              </Button>
+            </ListItem>
+          ))}
+          </List>
+        </Card>
+        <Card className='p-1'>
+          <List>
+            {Array.from(availableIngredients.entries()).map(([key, value]) => (
+              <ListItem key={key}>
+                {key} x{value}
+                <Button variant="outlined" 
+                onClick={() => {
+                  decreasePair(key);
+                }}
+                  sx={{borderRadius: 10}}>
+                  -
+                </Button>
+                <Button variant="outlined"
+                onClick={() => {
+                  deletePair(key);
+                }}>
+                  Delete
+                </Button>
+              </ListItem>
+            ))}
+          </List>
+        </Card>
+        <div className="p-1">
+          {displayRecipes().map((recipe, index) => (
+            <Card className='p-10 flex flex-column' key={index}>
+              <h1 className="text-2xl">{recipe}</h1>
+              <div className='flex flex-row'>
+                <Image
+                  alt="ingredient"
+                  src={`/${recipes.get(recipe)[0]}.jpeg`}
+                  height={64}
+                  width={64}/>
+                <Image
+                  alt="ingredient"
+                  src={`/${recipes.get(recipe)[1]}.jpeg`}
+                  height={64}
+                  width={64}/>
+                <Image
+                  alt="ingredient"
+                  src={`/${recipes.get(recipe)[2]}.jpeg`}
+                  height={64}
+                  width={64}/>
+              </div>
+            </Card>
+          ))}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   )
