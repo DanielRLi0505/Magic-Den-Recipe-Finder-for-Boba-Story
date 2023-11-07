40dc20f7a227ff025e00f46e4ff2ad9dbae5e43c
SoraHirasawa <89620534+SoraHirasawa@users.noreply.github.com>
2023-11-07 13:37:06 -0500

    Can now search by letter rather than by ingredient, crisper icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 'use client';
 
 import Image from 'next/image'
-import { Button, Card, List, ListItem, ListItemText, Paper } from '@mui/material';
+import { AppBar, Box, Button, Card, List, ListItem, ListItemText, Paper, Tab, Tabs, Typography } from '@mui/material';
 import { red } from '@mui/material/colors';
-import { useState } from 'react';
+import { SyntheticEvent, useState } from 'react';
 
 const ingredients = ["Bee", "Bear", "Chick", "Cloud", "Dino", "Flower", "Frog", "Heart", "Milk", "Milk (Strawberry)", "Mushroom", "Pig",
                    "Pineapple", "Planet", "Soda", "Star", "Sugar", "Tapioca"];
 
 const recipes = new Map();
+
+const alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 recipes.set("Autumn Leaf Boba", ["Cloud", "Flower", "Tapioca"]);
 recipes.set("Bear Boba", ["Bear", "Bear", "Tapioca"]);
@@ -22,7 +24,7 @@ recipes.set("Chick Boba", ["Chick", "Chick", "Tapioca"]);
 recipes.set("Cloud Boba", ["Cloud", "Cloud", "Tapioca"]);
 recipes.set("Cow Boba", ["Chick", "Milk", "Tapioca"]);
 recipes.set("Deer Boba", ["Mushroom", "Flower", "Tapioca"]);
-recipes.set("Diamond Boba", ["Planot", "Dino", "Tapioca"]);
+recipes.set("Diamond Boba", ["Planet", "Dino", "Tapioca"]);
 recipes.set("Dinosaur Boba", ["Dino", "Dino", "Tapioca"]);
 recipes.set("Dragon Boba", ["Cloud", "Dino", "Tapioca"]);
 recipes.set("Dragon Cream", ["Dino", "Cloud", "Milk"]);
@@ -49,7 +51,7 @@ recipes.set("Oreo Creme Brulee", ["Sugar", "Milk", "Milk"]);
 recipes.set("Panda Bear Boba", ["Bear", "Bee", "Tapioca"]);
 recipes.set("Pig Boba", ["Pig", "Pig", "Tapioca"]);
 recipes.set("Pineapple", ["Pineapple", "Pineapple", "Pineapple"]);
-recipes.set("Pineapple Boba", ["Pineapple", "Pineapple", "Boba"]);
+recipes.set("Pineapple Boba", ["Pineapple", "Pineapple", "Tapioca"]);
 recipes.set("Pineapple Jelly", ["Pineapple", "Pineapple", "Sugar"]);
 recipes.set("Pink Dinosaur Boba", ["Dino", "Heart", "Tapioca"]);
 recipes.set("Polar Bear Boba", ["Bear", "Soda", "Tapioca"]);
@@ -75,15 +77,90 @@ recipes.set("Strawberry Milk", ["Milk (Strawberry)", "Milk (Strawberry)", "Milk 
 recipes.set("Strawberries & Whipped Cream", ["Milk (Strawberry)", "Flower", "Sugar"]);
 recipes.set("Sunflower Boba", ["Star", "Flower", "Tapioca"]);
 recipes.set("Unicorn Boba", ["Soda", "Tapioca", "Soda"]);
-recipes.set("White Bunny Boba", ["Cloud", "Sugar", "Ramune"]);
+recipes.set("White Bunny Boba", ["Cloud", "Sugar", "Soda"]);
 recipes.set("White Cat Boba", ["Soda", "Tapioca", "Heart"]);
 recipes.set("Whipped Cream", ["Milk", "Milk", "Milk"]);
 
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: number;
+  value: number;
+}
 
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+interface RecipeProps {
+  name: string;
+  index: number;
+  ingredient1: string;
+  ingredient2: string;
+  ingredient3: string;
+}
+
+function Recipe(props: RecipeProps) {
+  const {name, index, ingredient1, ingredient2, ingredient3} = props;
+
+  return (
+    <Card className='p-10 flex flex-column' key={index}>
+      <h1 className="text-2xl">{name}</h1>
+      <div className='flex flex-row'>
+        <img
+          alt={ingredient1}
+          src={`/icons/${ingredient1}.JPEG`}
+          height={64}
+          width={64}
+          style={{ minWidth: 64, maxWidth: 64, minHeight: 64, maxHeight: 64}}/>
+        <img
+          alt={ingredient2}
+          src={`/icons/${ingredient2}.JPEG`}
+          height={64}
+          width={64}
+          style={{ minWidth: 64, maxWidth: 64, minHeight: 64, maxHeight: 64}}/>
+        <img
+          alt={ingredient3}
+          src={`/icons/${ingredient3}.JPEG`}
+          height={64}
+          width={64}
+          style={{ minWidth: 64, maxWidth: 64, minHeight: 64, maxHeight: 64}}/>
+      </div>
+    </Card>
+  )
+}
 
 export default function Home() {
   const [availableIngredients, setIngredients] = useState<Map<string, number>>(new Map());
+  const [tab, setTab] = useState(0);
+  const [letter, setLetter] = useState('!');
+  const ingredientsTab = () => {
+    setTab(0);
+  }
+  const handleTabs = (event: React.SyntheticEvent, newValue: number) => {
+    setTab(newValue);
+  };
+
+  const handleAlphabetTabs = (event: React.SyntheticEvent, newValue: string) => {
+    setLetter(newValue);
+  };
 
   // Function to add a new key-value pair
   const addPair = (key: string, value: number) => {
@@ -161,94 +238,155 @@ export default function Home() {
   
     return availableRecipes.filter((recipe) => recipe !== null);
   }
+
+  const displayRecipesByName = () => {
+    const availableRecipes = Array.from(recipes).map(([recipeName, ingredients]) => {
+      if (recipeName.charAt(0) == letter.charAt(0)) {
+        return recipeName;
+      }
+      return null;
+    });
+  
+    return availableRecipes.filter((recipe) => recipe !== null);
+  }
+
+  const displayAllRecipes = () => {
+    const availableRecipes = Array.from(recipes).map(([recipeName, ingredients]) => {
+      return recipeName
+    });
+  
+    return availableRecipes.filter((recipe) => recipe !== null);
+  }
   
   
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <title>Magic Den Recipe Finder</title>
-      <div className='fixed top-0 left-0 p-10 text-4xl font-sans'>
-        <h1> Magic Den Recipe Finder</h1>
-      </div>
-      <div className='absolute overflow-y-scroll fixed left-0 p-10 flex flex-row'>
-        <Card className='p-1'>
-          <List>
-            {ingredients.map((ingredient) => (
-            <ListItem className="p-5" key={ingredient}>
-              <img
-                  alt={ingredient}
-                  src={`/icons/${ingredient}.JPEG`}
-                  height={32}
-                  width={32}/>
-              {ingredient}
-              <Button
-                onClick={() => {
-                  if (availableIngredients.has(ingredient)) {
-                    updatePair(ingredient);
-                  }
-                  else {
-                    addPair(ingredient, 1);
-                  }
-                }} 
-                variant="outlined"
-                sx={ {borderRadius: 10}}>
-                +
-              </Button>
-            </ListItem>
-          ))}
-          </List>
-        </Card>
-        <Card className='p-1'>
-          <List>
-            {Array.from(availableIngredients.entries()).map(([key, value]) => (
-              <ListItem key={key}>
-                <img
-                  alt={key}
-                  src={`/icons/${key}.JPEG`}
-                  height={32}
-                  width={32}/>
-                {key} x{value}
-                <Button variant="outlined" 
-                onClick={() => {
-                  decreasePair(key);
-                }}
-                  sx={{borderRadius: 10}}>
-                  -
-                </Button>
-                <Button variant="outlined"
-                onClick={() => {
-                  deletePair(key);
-                }}>
-                  Delete
-                </Button>
-              </ListItem>
-            ))}
-          </List>
-        </Card>
-        <div className="p-1">
-          {displayRecipes().map((recipe, index) => (
-            <Card className='p-10 flex flex-column' key={index}>
-              <h1 className="text-2xl">{recipe}</h1>
-              <div className='flex flex-row'>
-                <img
-                  alt={recipes.get(recipe)[0]}
-                  src={`/icons/${recipes.get(recipe)[0]}.JPEG`}
-                  height={64}
-                  width={64}/>
-                <img
-                  alt={recipes.get(recipe)[1]}
-                  src={`/icons/${recipes.get(recipe)[1]}.JPEG`}
-                  height={64}
-                  width={64}/>
-                <img
-                  alt={recipes.get(recipe)[2]}
-                  src={`/icons/${recipes.get(recipe)[2]}.JPEG`}
-                  height={64}
-                  width={64}/>
-              </div>
-            </Card>
-          ))}
+      <div className='flex flex-col'>
+        <div className='fixed top-0 left-0 p-10 text-4xl font-sans'>
+          <h1> Magic Den Recipe Finder</h1>
         </div>
+        
+      </div>
+      
+      
+      <div className='absolute overflow-y-scroll fixed left-0 p-10 flex flex-col'>
+        <Tabs
+        value={tab}
+        onChange={handleTabs}
+        textColor="secondary"
+        indicatorColor="secondary"
+        aria-label="secondary tabs example"
+        >
+          <Tab value={0} label="ingredients"/>
+          <Tab value={1} label="recipes"/>
+        </Tabs>
+        {tab==0 && 
+          <div className='flex flex-row'>
+            <Card className='p-1'>
+              <List>
+                {ingredients.map((ingredient) => (
+                <ListItem className="p-5" key={ingredient}>
+                  <img
+                      alt={ingredient}
+                      src={`/icons/${ingredient}.JPEG`}
+                      height={32}
+                      width={32}/>
+                  {ingredient}
+                  <Button
+                    onClick={() => {
+                      if (availableIngredients.has(ingredient)) {
+                        updatePair(ingredient);
+                      }
+                      else {
+                        addPair(ingredient, 1);
+                      }
+                    }} 
+                    variant="outlined"
+                    sx={ {borderRadius: 10}}>
+                    +
+                  </Button>
+                </ListItem>
+              ))}
+              </List>
+            </Card>
+            <Card className='p-1'>
+              <List>
+                {Array.from(availableIngredients.entries()).map(([key, value]) => (
+                  <ListItem key={key}>
+                    <img
+                      alt={key}
+                      src={`/icons/${key}.JPEG`}
+                      height={32}
+                      width={32}/>
+                    {key} x{value}
+                    <Button variant="outlined" 
+                    onClick={() => {
+                      decreasePair(key);
+                    }}
+                      sx={{borderRadius: 10}}>
+                      -
+                    </Button>
+                    <Button variant="outlined"
+                    onClick={() => {
+                      deletePair(key);
+                    }}>
+                      Delete
+                    </Button>
+                  </ListItem>
+                ))}
+              </List>
+            </Card>
+            <div className="p-1">
+              {displayRecipes().map((recipe, index) => (
+                <Recipe
+                name={recipe}
+                index={index}
+                ingredient1={recipes.get(recipe)[0]}
+                ingredient2={recipes.get(recipe)[1]}
+                ingredient3={recipes.get(recipe)[2]}/>
+              ))}
+            </div>
+          </div>
+        }
+        {tab==1 && 
+          <div>
+            <Tabs
+            value={tab}
+            onChange={handleAlphabetTabs}
+            textColor="secondary"
+            indicatorColor="secondary"
+            aria-label="secondary tabs example"
+            >
+              <Tab value='!' label="All" style={{minWidth: 50}}/>
+              {Array.from(alphabet).map((letter) => (
+                <Tab value={letter} label={letter} style={{ minWidth: 50 }}/>
+              ))}
+            </Tabs>
+            {letter=="!" &&
+            <div>
+              {displayAllRecipes().map((recipe, index) => (
+                <Recipe
+                name={recipe}
+                index={index}
+                ingredient1={recipes.get(recipe)[0]}
+                ingredient2={recipes.get(recipe)[1]}
+                ingredient3={recipes.get(recipe)[2]}/>
+              ))}
+            </div>
+            }
+            {displayRecipesByName().map((recipe, index) => (
+              <Recipe
+              name={recipe}
+              index={index}
+              ingredient1={recipes.get(recipe)[0]}
+              ingredient2={recipes.get(recipe)[1]}
+              ingredient3={recipes.get(recipe)[2]}/>
+            ))}
+          </div>}
+        
+        
       </div>
     </main>
   )
